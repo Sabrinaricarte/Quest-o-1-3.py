ac9fe2e3a6c6cb3f979dfcf580fced91d248c5f9
Sabrinaricarte <113310723+Sabrinaricarte@users.noreply.github.com>
2022-11-08 10:45:28 -0300

    Add files via upload

---
+++ questão 1.py
@@ -0,0 +1,24 @@
+numeros = []
+num = 0
+quantidadedePares = 0
+quantidadeImpares = 0
+epar = 0 
+
+for n in range (0,6):
+     numeros.append = (int(input("informe o número:")))
+for n in range (0,6):
+     numeros.append
+if numeros [n] % 2 == 0:
+     print(numeros)[n]
+     epar += 1
+     print(epar)
+
+
+
+
+    
+
+
+
+
+
